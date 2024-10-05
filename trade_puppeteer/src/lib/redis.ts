4cf6d9c8e81aed3293291296514af62a05a0451f
Kiyoonewton <Kiyoonewton41@gmail.com>
2024-10-05 12:59:40 +0100

    add redis-client

--- trade_puppeteer/src/lib/redis.ts
+++ trade_puppeteer/src/lib/redis.ts
@@ -1,12 +1,21 @@
-import redis from 'redis';
+import { createClient } from 'redis';
 
 // Create a Redis client
-export const redisClient = redis.createClient({
+export const redisClient = createClient({
   url: 'redis://127.0.0.1:6379',  // Default Redis port
 });
 
 // Connect to Redis
-redisClient.connect();
+// redisClient.connect();
+
+
+// redisClient.connect()
+//   .then(() => {
+//     console.log('Connected to Redis successfully!');
+//   })
+//   .catch((err) => {
+//     console.error('Failed to connect to Redis:', err);
+//   });
 
 redisClient.on('connect', () => {
   console.log('Connected to Redis');
