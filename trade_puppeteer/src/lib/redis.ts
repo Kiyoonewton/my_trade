7608334779312f297efa825ae53b5458a94929ea
Kiyoonewton <Kiyoonewton41@gmail.com>
2024-10-05 13:01:28 +0100

    remove dead code

--- trade_puppeteer/src/lib/redis.ts
+++ trade_puppeteer/src/lib/redis.ts
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 
 // Create a Redis client
 export const redisClient = createClient({
-  url: 'redis://127.0.0.1:6379',  // Default Redis port
+  url: 'redis://127.0.0.1:6379',
 });
 
 redisClient.on('connect', () => {
